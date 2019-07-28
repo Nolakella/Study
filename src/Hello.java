90f8c8bd03420be82066e341bff2ad2f5aef6ee4
Nolakella <nast>
2019-07-28 13:50:47 +0400

    First comment

---
+++ src/Hello.java
@@ -0,0 +1,17 @@
+public class Hello {
+    public static void main(String[]args ){
+        int myInt;
+        myInt=557;
+        short myShort= 3266;
+        long myLong=6795798;
+        double myDouble = 567.8;
+        float myFloat = 2345.8f;
+        char myChar = 'k';
+        boolean myBoo=true;
+        byte myB = 127; //-128 - 127
+        System.out.println(myInt);
+    }
+
+
+
+}
